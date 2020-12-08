3f819014ddaf1def8a86e5a64279c163c9cc981f
John Suarez-Perez <jf.suarez@uniandes.edu.co>
2020-12-08 15:41:03 -0800

    fiberassign 20201208

--- command.sh
+++ command.sh
@@ -1,1 +1,11 @@
 python fba_sv1.py --dr dr9m --dtver 0.45.1 --rundate 2020-03-06T00:00:00 --seed 77  --tilera 36.448 --tiledec -4.601 --tileid 85000 --flavor scidark --outdir sv1_tiles_20201208/
+
+python fba_sv1.py --dr dr9m --dtver 0.45.1 --rundate 2020-03-06T00:00:00 --seed 77  --tilera 115 --tiledec +50 --tileid 85001 --flavor scidark --outdir sv1_tiles_20201208/
+
+python fba_sv1.py --dr dr9m --dtver 0.45.1 --rundate 2020-03-06T00:00:00 --seed 77  --tilera 150.119166667 --tiledec +2.20583333 --tileid 85002 --flavor scidark --outdir sv1_tiles_20201208/
+
+python fba_sv1.py --dr dr9m --dtver 0.45.1 --rundate 2020-03-06T00:00:00 --seed 77  --tilera 36.448 --tiledec -4.601 --tileid 85003 --flavor scibright --outdir sv1_tiles_20201208/
+
+python fba_sv1.py --dr dr9m --dtver 0.45.1 --rundate 2020-03-06T00:00:00 --seed 77  --tilera 115 --tiledec +50 --tileid 85004 --flavor scibright --outdir sv1_tiles_20201208/
+
+python fba_sv1.py --dr dr9m --dtver 0.45.1 --rundate 2020-03-06T00:00:00 --seed 77  --tilera 150.119166667 --tiledec +2.20583333 --tileid 85005 --flavor scibright --outdir sv1_tiles_20201208/
